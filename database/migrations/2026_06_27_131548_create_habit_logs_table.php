fb0a79090ac96a8fad2ed972e4d5329d4b9a9975
Lucio-Gabriel <luciogabrieldeazevedoo@gmail.com>
2026-06-27 10:42:24 -0300

    create model and migration for habit and habitLog

---
+++ database/migrations/2026_06_27_131548_create_habit_logs_table.php
@@ -0,0 +1,25 @@
+<?php
+
+use App\Models\Habit;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('habit_logs', function (Blueprint $table) {
+            $table->id();
+            $table->string('uuid')->unique();
+            $table->foreignIdFor(Habit::class)->constrained();
+            $table->dateTime('completed_at');
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('habit_logs');
+    }
+};
